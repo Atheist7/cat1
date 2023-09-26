4859a0fbba9213347789175af6e328fdce627f9b
Atheist7 <141166851+Atheist7@users.noreply.github.com>
2023-09-26 21:09:26 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
+# This is a sample Python script.
 
-import telebot
-from config import keys, TOKEN
-from extensions import APIException, CryptoConverter
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start', 'help'])
-def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу введите команду боту в следующнм формате:\n<имя валюты цену которой он хочет узнать> \
-<имя валюты в которой надо узнать цену первой валюты> \
-<количество первой валюты>\nУвидеть список всех доступных валют: /values'
-    bot.reply_to(message, text)
-
-@bot.message_handler(commands=['values'])
-def values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
-    for key in keys.keys():
-        text = '\n'.join((text, key, ))
-    bot.reply_to(message, text)
-
-@bot.message_handler(content_types=['text', ])
-def convert(message: telebot.types.Message):
-    try:
-        values = message.text.split(' ')
-
-        if len(values) > 3:
-            raise APIException("Слишком много параметров.")
-        elif len(values) < 3:
-            raise APIException("Мало параметров.")
-
-        base, quote, amount = values
-        total_base = CryptoConverter.get_price(base, quote, amount)
-    except APIException as e:
-        bot.reply_to(message, f'Ошибка пользователя\n{e}')
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
-    else:
-        text = f'Цена {amount} {base} в {quote} - {total_base}'
-        bot.send_message(message.chat.id, text)
-
-bot.polling()
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
